42770a6c0a841b2cf45bb4eeccff95e3cd9bcac9
Samuel Fox <samueljasonfox@gmail.com>
2021-12-02 02:00:11 -0800

    update naming conventions

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const textareaEl = select('textarea')
 formEl.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const destionation = addressEl.value
-    const action = e.submitter.dataset.format
+    const destination = addressEl.value
+    const renderAs = e.submitter.dataset.format
     const subject = 'hello from https://samuelfox1.github.io/emailer-client/'
     const body = `${textareaEl.value} \n\n ${subject}`
 
-    if (!action || !destionation || !body) return
+    if (!renderAs || !destination || !body) return
 
     const url = 'https://sf-emailer.herokuapp.com/api/email'
 
@@ -20,7 +20,7 @@ formEl.addEventListener('submit', (e) => {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ destionation, action, subject, body }),
+        body: JSON.stringify({ destination, subject, renderAs, body }),
     })
         .then(response => response.json())
         .then(data => console.log(data))
